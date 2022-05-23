a744b2518ea1f56b07595a08c56387ef74898d8f
Sulav <sulavshrestha023@gmail.com>
2022-05-23 18:02:23 +0545

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import data from "./data";
+
 function App() {
-  return <h1>This is App</h1>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = Number(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h3>Random Paragraph Generator</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraph</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">Generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default App;
